2802ace324c88dbe98ea054c6c015f9e8999c013
Jay Krell <jay.krell@cornell.edu>
2025-05-27 22:42:05 -0700

    Start with the problem statement.

---
+++ stock.cpp
@@ -0,0 +1,12 @@
+/*
+Question statement
+# Given predicted stock prices for next n days for a stock, find the maximum
+# profit that can be made with a single buy-sell transaction (e.g. you can buy
+# single share of stock on one day, and then sell it on a subsequent day).  If
+# no profit can be made return 0.
+
+
+# For example, given this list of daily stock predictions:
+# (20, 40, 52, 15, 18, 50, 20, 45),
+# buying at 15 and selling at 50 gives maximum profit of 35.
+*/
